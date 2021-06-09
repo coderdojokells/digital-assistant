b6790ea9419cd79d51fdcb7b7d0ab40d3411ce9c
Peter Heylin <peter@peterheylin.com>
2021-06-09 17:58:40 +0100

    20210609 added basic machine learning functionality

--- DigitalAssistant.py
+++ DigitalAssistant.py
@@ -5,17 +5,28 @@ def main():
     print("Welcome, please enter your name: ")
     userName = input()
     existingUser = selectUser(userName)
-    existingUser = existingUser[0]
-    
-    if userName == existingUser:
-        print("Welcome back " + existingUser)
+    print(existingUser)
+    if existingUser is not None:
+        existingUserName = existingUser[1]
+
+    if existingUser is not None:
+        if userName == existingUserName:
+            print("Welcome back " + existingUserName)
+            print("So, your job is " + existingUser[3])
+            print("What do you do in your job?")
+            activity = input()
+            saveJobActivity(existingUser[3], activity)
         
-    print("Thank you " + userName + ". What is your age?")
-    userAge = input()
-    print("Thank you " + userName + ". What is your job?")
-    userJob = input()    
-    saveUser(userName, userAge, userJob)
-    
+    else:
+        print("Thank you " + userName + ". What is your age?")
+        userAge = input()
+        print("Thank you " + userName + ". What is your job?")
+        userJob = input()    
+        saveUser(userName, userAge, userJob)
+        existingJob = selectJob(userJob)
+        print("So, you work as a " + userJob)
+        print("It sounds like in your job, you " + existingJob[2])
+            
 
 def saveUser(name, age, job):
     # link to the database file or creates one if it does not exist
@@ -23,17 +34,18 @@ def saveUser(name, age, job):
     sql_create_user_table = """ CREATE TABLE IF NOT EXISTS user (
                                         id integer PRIMARY KEY,
                                         name text NOT NULL,
-                                        age integer NOT NULL
+                                        age integer NOT NULL,
                                         job text NOT NULL
     
                                    ); """
+    
     
     # create connection to database
     conn = createConn(database)
     
     if conn is not None:
         # create user table if one does not exist already 
-        createTable(conn, sql_create_user_table)      
+        createTable(conn, sql_create_user_table)
     else:
         print('Error! cannot create the database connection')
 
@@ -41,21 +53,33 @@ def saveUser(name, age, job):
     with conn:
         user = (name, age, job)
         userID = insertUser(conn, user)
-        #print(reminderID)     
     conn.close()
 
 
-# this function is used select a single reminder from the database for a given time
+# this function is used select a single user from the database 
 def selectUser(name):
     database = r"db\assistant.db"
     # create connection to database
     conn = createConn(database)
     
     cur = conn.cursor()
-    cur.execute("SELECT name FROM user WHERE name=? LIMIT 1", (name,))
+    cur.execute("SELECT * FROM user WHERE name=? LIMIT 1", (name,))
 
     user = cur.fetchone()
     return user
+
+
+# this function is used select a single job from the database 
+def selectJob(job):
+    database = r"db\assistant.db"
+    # create connection to database
+    conn = createConn(database)
+    
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM activities WHERE job=? LIMIT 1", (job,))
+
+    job = cur.fetchone()
+    return job
 
 
 # this function retrieves all stored users 
@@ -82,6 +106,47 @@ def insertUser(conn, user):
     cur.execute(sql, user)
     conn.commit()
     return cur.lastrowid
+
+
+# writes the user details to the database
+def insertActivity(conn, activity):
+    sql = ''' INSERT INTO activities(job ,activity)
+              VALUES(?,?)
+    
+            '''
+    cur = conn.cursor()
+    cur.execute(sql, activity)
+    conn.commit()
+    return cur.lastrowid
+
+
+# write job activities to database to learn about jobs
+def saveJobActivity(job, activity):
+    # link to the database file or creates one if it does not exist
+    database = r"db\assistant.db"
+
+    sql_create_activity_table = """ CREATE TABLE IF NOT EXISTS activities (
+                                        id integer PRIMARY KEY,
+                                        job text NOT NULL,
+                                        activity text NOT NULL
+    
+                                   ); """
+
+    
+    # create connection to database
+    conn = createConn(database)
+    
+    if conn is not None:
+        # create user table if one does not exist already 
+        createTable(conn, sql_create_activity_table) 
+    else:
+        print('Error! cannot create the database connection')
+
+
+    with conn:
+        jobActivity = (job, activity)
+        activityID = insertActivity(conn, jobActivity)
+    conn.close()
 
 
 # creates a connection to the database 
